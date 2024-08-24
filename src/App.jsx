8f36f96cae46ab28e8f1c84a3d0d2be6e1e6dfdf
saurabh <kr.saurabh3008@gmail.com>
2024-08-24 11:24:20 +0530

    made component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
  
 
   return (
     <>
-     <p className='text-center'> saurabh</p>
+    <Navbar />
     </>
   )
 }
